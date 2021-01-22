b69782c6fdbff020e4b3018060d5ba5d28237db8
Jerome van den Wyenbergh <jeyvdw@outlook.de>
2021-01-23 00:37:58 +0100

    CoreSponge revised

--- sponge/src/main/java/dev/minecode/core/sponge/CoreSponge.java
+++ sponge/src/main/java/dev/minecode/core/sponge/CoreSponge.java
@@ -3,12 +3,23 @@ package dev.minecode.core.sponge;
 import dev.minecode.core.common.CoreCommon;
 
 public class CoreSponge {
-
     private static CoreSponge instance;
 
+    private CoreCommon coreCommon;
+
+    private String pluginName;
+    private String pluginVersion;
+
     public CoreSponge(String pluginName, String pluginVersion) {
+        this.pluginName = pluginName;
+        this.pluginVersion = pluginVersion;
+
+        makeInstances();
+    }
+
+    private void makeInstances() {
         instance = this;
-        new CoreCommon(pluginName, pluginVersion);
+        coreCommon = new CoreCommon(pluginName, pluginVersion);
     }
 
     public static CoreSponge getInstance() {
