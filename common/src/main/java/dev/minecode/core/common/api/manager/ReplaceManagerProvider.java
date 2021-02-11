943369cde17becb2759e87cfd2e60fdb294eb3dc
Jerome van den Wyenbergh <jeyvdw@outlook.de>
2021-02-12 00:21:33 +0100

    ChatColor aus ReplaceManagerProvider entfernt; 0.1.0-Pre.27

--- common/src/main/java/dev/minecode/core/common/api/manager/ReplaceManagerProvider.java
+++ common/src/main/java/dev/minecode/core/common/api/manager/ReplaceManagerProvider.java
@@ -44,53 +44,54 @@ public class ReplaceManagerProvider implements ReplaceManager {
 
     @Override
     public ReplaceManager chatcolorColor() {
-        return replaceAll("&1", ChatColor.COLOR_CHAR + "1")
-                .replaceAll("&2", ChatColor.COLOR_CHAR + "2")
-                .replaceAll("&3", ChatColor.COLOR_CHAR + "3")
-                .replaceAll("&4", ChatColor.COLOR_CHAR + "4")
-                .replaceAll("&5", ChatColor.COLOR_CHAR + "5")
-                .replaceAll("&6", ChatColor.COLOR_CHAR + "6")
-                .replaceAll("&7", ChatColor.COLOR_CHAR + "7")
-                .replaceAll("&8", ChatColor.COLOR_CHAR + "8")
-                .replaceAll("&9", ChatColor.COLOR_CHAR + "9")
-                .replaceAll("&a", ChatColor.COLOR_CHAR + "a")
-                .replaceAll("&A", ChatColor.COLOR_CHAR + "a")
-                .replaceAll("&b", ChatColor.COLOR_CHAR + "b")
-                .replaceAll("&B", ChatColor.COLOR_CHAR + "b")
-                .replaceAll("&c", ChatColor.COLOR_CHAR + "c")
-                .replaceAll("&C", ChatColor.COLOR_CHAR + "c")
-                .replaceAll("&d", ChatColor.COLOR_CHAR + "d")
-                .replaceAll("&D", ChatColor.COLOR_CHAR + "d")
-                .replaceAll("&e", ChatColor.COLOR_CHAR + "e")
-                .replaceAll("&E", ChatColor.COLOR_CHAR + "e")
-                .replaceAll("&f", ChatColor.COLOR_CHAR + "f")
-                .replaceAll("&F", ChatColor.COLOR_CHAR + "f");
+        return replaceAll("&1", "§1")
+                .replaceAll("&2", "§2")
+                .replaceAll("&3", "§3")
+                .replaceAll("&4", "§4")
+                .replaceAll("&5", "§5")
+                .replaceAll("&6", "§6")
+                .replaceAll("&7", "§7")
+                .replaceAll("&8", "§8")
+                .replaceAll("&9", "§9")
+                .replaceAll("&a", "§a")
+                .replaceAll("&A", "§a")
+                .replaceAll("&b", "§b")
+                .replaceAll("&B", "§b")
+                .replaceAll("&c", "§c")
+                .replaceAll("&C", "§c")
+                .replaceAll("&d", "§d")
+                .replaceAll("&D", "§d")
+                .replaceAll("&e", "§e")
+                .replaceAll("&E", "§e")
+                .replaceAll("&f", "§f")
+                .replaceAll("&F", "§f");
     }
 
     @Override
     public ReplaceManager chatcolorFormat() {
-        return replaceAll("&l", ChatColor.COLOR_CHAR + "l")
-                .replaceAll("&L", ChatColor.COLOR_CHAR + "l")
-                .replaceAll("&m", ChatColor.COLOR_CHAR + "m")
-                .replaceAll("&M", ChatColor.COLOR_CHAR + "m")
-                .replaceAll("&n", ChatColor.COLOR_CHAR + "n")
-                .replaceAll("&N", ChatColor.COLOR_CHAR + "n")
-                .replaceAll("&o", ChatColor.COLOR_CHAR + "o")
-                .replaceAll("&O", ChatColor.COLOR_CHAR + "o")
-                .replaceAll("&r", ChatColor.COLOR_CHAR + "r")
-                .replaceAll("&R", ChatColor.COLOR_CHAR + "r");
+        return replaceAll("&l", "§l")
+                .replaceAll("&L", "§l")
+                .replaceAll("&m", "§m")
+                .replaceAll("&M", "§m")
+                .replaceAll("&n", "§n")
+                .replaceAll("&N", "§n")
+                .replaceAll("&o", "§o")
+                .replaceAll("&O", "§o")
+                .replaceAll("&r", "§r")
+                .replaceAll("&R", "§r");
     }
 
     @Override
     public ReplaceManager chatcolorMagic() {
-        return replaceAll("&k", ChatColor.COLOR_CHAR + "k")
-                .replaceAll("&K", ChatColor.COLOR_CHAR + "k");
+        return replaceAll("&k", "§k")
+                .replaceAll("&K", "§k");
     }
 
     @Override
     public ReplaceManager chatcolorAll() {
-        this.message = ChatColor.translateAlternateColorCodes('&', this.message);
-        return this;
+        return chatcolorColor()
+                .chatcolorFormat()
+                .chatcolorFormat();
     }
 
     @Override
